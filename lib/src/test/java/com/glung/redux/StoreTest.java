bc87bcfba774d8b558ceb28f0d99e6abb1ec94fd
Guillaume Lung <guillaume@soundcloud.com>
2017-01-20 00:09:58 +0100

    Fix tests

--- lib/src/test/java/com/glung/redux/StoreTest.java
+++ lib/src/test/java/com/glung/redux/StoreTest.java
@@ -6,6 +6,6 @@ public class StoreTest extends redux.api.StoreTest {
 
     @Override
     public <S> redux.api.Store createStore(Reducer<S> reducer, S state) {
-        return new Store.Creator<S>().create(reducer, state);
+        return new Store.Creator().create(reducer, state);
     }
 }
