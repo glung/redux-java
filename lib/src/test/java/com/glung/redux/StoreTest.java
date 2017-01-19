ab3e64b6ec098b4d250246d2502107a949cc1bfb
Guillaume Lung <guillaume@soundcloud.com>
2017-01-20 00:09:58 +0100

    Fix tests

--- lib/src/test/java/com/glung/redux/StoreTest.java
+++ lib/src/test/java/com/glung/redux/StoreTest.java
@@ -1,11 +1,13 @@
 package com.glung.redux;
 
+import org.jetbrains.annotations.NotNull;
 import redux.api.Reducer;
 
 public class StoreTest extends redux.api.StoreTest {
 
+    @NotNull
     @Override
-    public <S> redux.api.Store createStore(Reducer<S> reducer, S state) {
-        return new Store.Creator<S>().create(reducer, state);
+    public <S> redux.api.Store<S> createStore(@NotNull Reducer<S> reducer, @NotNull S state) {
+        return new Store.Creator().create(reducer, state);
     }
 }
